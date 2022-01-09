c393e75ed19e2eda5ac9b0c5fc917650aee2e555
Ramon-darwin <RAMON.DARWIN@iisgalvanimi.edu.it>
2022-01-09 14:28:35 +0000

    punto 2

---
+++ prova-spammer/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'prova-spammer';
+  articles = new Array<string>();
+  addSpam(testo : HTMLInputElement,numero : HTMLInputElement) : boolean   
+  {
+    let t = testo.value; 
+    let n = Number(numero.value); 
+    for (let index = 0; index < n; index++) {
+      this.articles.push(t);
+      
+    }
+    return false;
+  
+  }
+}
